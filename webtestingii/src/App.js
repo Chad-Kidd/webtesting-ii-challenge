539acbd86ce9fe2f88f1d9c187be8c561cd067d9
chad kidd <ckiddwebdev@gmail.com>
2019-05-14 14:52:55 -0700

    app created and test dependencies installed and working

---
+++ webtestingii/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Hello World!</h3>
+      {/* <div>
+        <button onClick={greet}>Greet</button>
+      </div>
+      <h4>{greeting}</h4> */}
+    </div>
+  );
+}
+
+export default App;
